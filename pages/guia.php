88fb2f793dbb2b6f123453d302767fbffb371bd7
Lucas Pereira <lucaspereira.dev@proton.me>
2024-08-19 14:38:15 -0300

    Ajustando favicon

--- pages/guia.php
+++ pages/guia.php
@@ -13,6 +13,7 @@ $guias = $result->fetch_all(MYSQLI_ASSOC);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="../assets/favicon.ico" type="image/x-icon">
   <link rel="stylesheet" href="../styles/style.css">
   <title>Guia Educacional</title>
 </head>
