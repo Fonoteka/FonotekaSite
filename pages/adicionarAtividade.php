8564fd8d0fd823a4d5919d22da37fce493a99729
Lucas Pereira <lucaspereira.dev@proton.me>
2024-08-19 14:58:45 -0300

    Junção página adicionarAtividades

---
+++ pages/adicionarAtividade.php
@@ -0,0 +1,86 @@
+<?php
+include_once("../php/session.php");
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../assets/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="../styles/style.css" />
+    <title> Adicionar Atividades </title>
+</head>
+<header>
+    <a class="link_img" href="./index.php">
+        <img class="img_logo" src="../assets/Logo.png" />Fonoteka
+    </a>
+
+    <div class="opcoes_div">
+        <a href="#"> Home </a>
+        <a href=""> Sobre nós </a>
+        <a href=""> Aluno </a>
+        <a href="./guia.php"> Guia </a>
+    </div>
+
+    <div class="div_usuario">
+        <img id="perfil_usuario" class="img_perfil"
+            src="<?php echo !empty($_SESSION['path_img']) ? $_SESSION['path_img'] : '../assets/perfil-Icon.png' ?>" />
+        <label for="perfil_usuario" class="perfil_label">
+            <?php echo !empty($_SESSION['id']) ? $_SESSION['nome'] : "Usuário"; ?></label>
+    </div>
+
+    <form class="popLogin" method="POST">
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required disabled>
+        <label for="senha">Senha:</label>
+        <input type="password" id="senha" name="senha" required disabled>
+        <input type="submit" value="Acessar">
+    </form>
+</header>
+
+<body>
+
+    <form method="POST" class="fundo">
+        <div class="containerimagem">
+            <img class="imagem" src="../assets/Adicionarativ.png">
+            <input class="bntadd" type="submit" value="Enviar Atividade">
+        </div>
+
+        <div class="containertexto">
+            <input class="titulotext" type="text" id="titulo" placeholder="Digite o nome da atividade:"
+                name="atividade">
+            <p class="obs"> Abaixo adicione as informações de forma curta para execução da atividade</p>
+            <input class="obsadd" type="text" placeholder="Adicionar descrição" name="atividade">
+
+        </div>
+
+        <div class="containeradicional">
+            <input class="pontos" type="number" placeholder="Quantidade de Pontos" name="pontos">
+            <input class="pontos" type="number" placeholder="Nível de Autismo" name="pontos">
+            <input class="pontos" type="date" placeholder="Data Inicial (Mentor)" name="pontos">
+            <input class="pontos" type="datetime-local" placeholder="Data Final (Aluno)" name="pontos">
+            <label class="audio"> Adicionar áudio/pdf:</label>
+            <input class="audio1" type="file" placeholder="Adicionar PDF" name="pontos">
+
+
+        </div>
+    </form>
+
+</body>
+
+<dialog>
+    <h1 id="msgCadastro"></h1>
+    <button id="buClose" class="buClose">Fechar</button>
+</dialog>
+
+<script src="../js/index.js"></script>
+
+<?php
+
+include('../php/conexao.php');
+include('../php/login.php');
+
+?>
+
+</html>
