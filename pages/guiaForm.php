107df4ba399be2a4d4df33fe9085d9b7a2f2f8ee
Lucas <bancodesanguetcc03@gmail.com>
2024-09-03 11:50:50 -0300

    estilo pagina `guiaForm.php`

--- pages/guiaForm.php
+++ pages/guiaForm.php
@@ -4,14 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../assets/favicon.ico" type="image/x-icon">
-    <style>
-        body {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            background-color: #c1e5f5;
-        }
-    </style>
+    <link rel="stylesheet" href="../styles/style.css">
 </head>
 
 <body>
